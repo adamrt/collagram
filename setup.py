b7c1aaa6981c16aebdd8772fc422b3c80817d4d1
Adam Patterson <adam@adamrt.com>
2013-02-13 22:00:42 -0600

    simplify setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 setup(
     name='collagram',
-    version='0.1.1',
-    url='http://github.com/vurbmedia/collagram/',
-    license='ISC',
+    version='0.2',
     author='Adam Patterson',
     author_email='adam@adamrt.com',
+    url='http://github.com/vurbmedia/collagram/',
+    license='ISC',
     description='Generate collages of Instagram photographs.',
-    packages=find_packages(),
+    packages=['collagram', 'collagram.test'],
     include_package_data=True,
     zip_safe=False,
     platforms='any',
